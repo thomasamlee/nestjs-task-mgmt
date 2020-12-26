ab422e5e70ac41dc1c8f0bd9d148644820cc2f6a
thomasamlee <thomasamlee@gmail.com>
2020-12-25 19:14:57 -0600

    task controller

---
+++ src/tasks/tasks.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@nestjs/common';
+import { v1 as uuid } from 'uuid';
+import { Task, TaskStatus } from './task.model';
+
+@Injectable()
+export class TasksService {
+  private tasks: Task[] = [];
+
+  getAllTasks() {
+    return this.tasks;
+  }
+
+  createTask(title: string, description: string): Task {
+    const task: Task = {
+      id: uuid(),
+      title,
+      description,
+      status: TaskStatus.OPEN,
+    };
+
+    this.tasks.push(task);
+
+    return task;
+  }
+}
